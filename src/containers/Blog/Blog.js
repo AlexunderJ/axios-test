3f15f229609f44a6030fae4878af4e884d44ed4c
AlexunderJ <pozdrower@wp.pl>
2020-11-18 13:54:00 +0100

    async import component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent'
+// import NewPost from './NewPost/NewPost';
 
+const AsyncNewPost = asyncComponent(()=>{
+    return import('./NewPost/NewPost');
+});
 
 // import axios from 'axios';
 // import axios from '../../axios';
 
-import './Blog.css';
 
 class Blog extends Component {
   
@@ -37,7 +41,7 @@ state = {
                 </header>
                {/* <Route path='/' exact render={()=><Posts/>} /> */}
                <Switch>
-               {this.state.auth ? <Route path='/new-post' component={NewPost} /> : null}
+               {this.state.auth ? <Route path='/new-post' component={AsyncNewPost} /> : null}
                <Route path='/posts'  component={Posts} />
                 <Route render={()=><h1>Not Found</h1>} />
                {/* <Redirect from='/' to='/posts'  /> */}
